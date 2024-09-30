dc6492ba430714dd652918f56c6ad8b1b3678e88
ViniiciusCarneiro <108013876+ViniiciusCarneiro@users.noreply.github.com>
2024-09-30 12:43:52 -0300

    Se a linha for igual a 0 não adicionar no excel

--- log_excel.py
+++ log_excel.py
@@ -2,6 +2,8 @@ import openpyxl
 from datetime import datetime
 
 def log_to_excel(file_name, table_name, rows_inserted):
+    if rows_inserted == 0:
+        return
     workbook = openpyxl.load_workbook(file_name)
     sheet = workbook.active
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
